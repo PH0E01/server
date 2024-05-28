1b531127c4649d8afecfab7abcde5f63334bca02
PH0E01 <phoetears01@gmail.com>
2024-05-28 14:56:10 +0300

    first commit

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,22 +9,21 @@ public class Server {
     public static void main(String[] args) {
         int port = 9090;
         try {
-            ServerSocket serverSocket = new ServerSocket(port); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket clientSocket = serverSocket.accept();
+
 
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             System.out.println("New connection accepted");
 
             final String name = in.readLine();
-
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
+            if (name != null) {
+                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,25 +1,40 @@
+
+import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Arrays;
 
-public class Client {
-    public static void main(String[] args) throws IOException {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ClientTest {
+
+
+    @Test
+    public void testClientConnecting() {
+        // given
         String localhost = "127.0.0.1";
         int port = 9090;
-        try (Socket clientSocket = new Socket(localhost, port)){
-            clientConnecting(clientSocket);
+
+        // when
+        boolean result;
+        try (Socket clientSocket = new Socket(localhost, port)) {
+            result = Client.clientConnecting(clientSocket);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        // then
+        assertTrue(result);
     }
 
     static boolean clientConnecting(Socket clientSocket) throws IOException {
         try (PrintWriter printOut = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            // Не отправляем имя клиента, так как сервер ожидает его
-            // printOut.println("ClientName");
+            // не отправляем имя клиента, так как сервер ожидает его
+            // printOut.println("clientName");
 
             String resp;
             while ((resp = in.readLine()) != null) {
@@ -30,6 +45,3 @@ public class Client {
         }
     }
 }
-
-
-
